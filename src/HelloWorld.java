84d57c102d1961c43a5883da764030fed60726f8
jasminerivera <jazzyrivera@yahoo.com>
2018-05-01 09:55:30 -0500

    Syntax, Types, and Variables exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // statement below prints "Hello, World!"
+        System.out.println("Hello, World!");
+        // ^^ double quotes used because this is a string
+        // create int with my fav number
+        int myFavoriteNumber = 22;
+        // print out fav number
+        System.out.println(myFavoriteNumber);
+        // create myString assign string value
+        //   - first declare it is a String
+        String myString;
+        //   - then initialize
+        myString = "I am a string!";
+        //   - and print
+        System.out.println(myString);
+        // declare a long variable myNumber
+        long myNumber;
+        // if I don't initialize, myNumber does not print
+        // when I try 3.14, I am alerted that I need to use double not long
+        myNumber = 3;
+        System.out.println(myNumber);
+
+
+    }
+}
