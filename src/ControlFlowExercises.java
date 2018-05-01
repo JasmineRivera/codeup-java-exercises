d02767ce428423622790cb7595d11f55e6bc3830
jasminerivera <jazzyrivera@yahoo.com>
2018-05-01 10:33:49 -0500

    Control Statements and Loops: exercise A.

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,11 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // create integer variable
+        int i = 5;
+        // while loop
+        while (i <= 15) {
+            System.out.println(i);
+                i++;
+        }
+    }
+}
