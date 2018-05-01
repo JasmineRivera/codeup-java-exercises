590d77bf03456a7a9416c1441db669191fe5b561
jasminerivera <jazzyrivera@yahoo.com>
2018-05-01 14:43:03 -0500

    Strings Exercise 1.b

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,7 +1,6 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String message = "We don't need no education\n" +
-                "We don't need no thought control";
+        String message = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(message);
     }
 }
